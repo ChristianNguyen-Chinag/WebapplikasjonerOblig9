25c04e323941df08026210e3c980b2be1772d4f5
Christian Nguyen <60467963+ChristianNguyen-Chinag@users.noreply.github.com>
2021-11-01 14:46:40 +0100

    Added Layout and Navigation components

---
+++ my-next-app/components/Navigation.js
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+
+const Navigation = () => {
+  return (
+    <nav className="navigation">
+      <ul>
+        <li>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/trivia">
+            <a>Trivia</a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navigation
